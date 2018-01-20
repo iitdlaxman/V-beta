eb13232648f28f67b9e50325cc4af90fa2354e32
Falcon66 <rahul1281994@gmail.com>
2018-01-20 21:02:02 +0530

    Test Push

--- app/src/main/java/com/vikshy/v_beta/Home.java
+++ app/src/main/java/com/vikshy/v_beta/Home.java
@@ -32,6 +32,8 @@ public class Home extends AppCompatActivity
             }
         });
 
+        //lol
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
