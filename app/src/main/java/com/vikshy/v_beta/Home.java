08013c6d49da6893913daf404d2ec2e3a957f85c
Laxman <laxman.muttineni@flipkart.com>
2018-01-26 12:24:37 +0530

    working banner sliding using banner_slider

--- app/src/main/java/com/vikshy/v_beta/Home.java
+++ app/src/main/java/com/vikshy/v_beta/Home.java
@@ -13,6 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ss.com.bannerslider.banners.Banner;
+import ss.com.bannerslider.banners.DrawableBanner;
+import ss.com.bannerslider.banners.RemoteBanner;
+import ss.com.bannerslider.events.OnBannerClickListener;
+import ss.com.bannerslider.views.BannerSlider;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -45,6 +55,26 @@ public class Home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        BannerSlider bannerSlider = (BannerSlider) findViewById(R.id.banner_slider1);
+        List<Banner> banners=new ArrayList<>();
+        //add banner using image url
+        banners.add(new RemoteBanner("http://androidblog.esy.es/images/cupcake-1.png"));
+        banners.add(new RemoteBanner(
+                "https://assets.materialup.com/uploads/dcc07ea4-845a-463b-b5f0-4696574da5ed/preview.jpg"
+        ));
+        //add banner using resource drawable
+        banners.add(new DrawableBanner(R.drawable.abc_btn_radio_material));
+        bannerSlider.setBanners(banners);
+
+        bannerSlider.setOnBannerClickListener(new OnBannerClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(Home.this, "Banner with position " + String.valueOf(position) + " clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
